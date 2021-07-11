2da14db9af80c46fe8205242ea2e0ac31193b34e
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-07-11 12:56:46 -0300

    added loading and message component

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,10 @@
+import 'font-awesome/css/font-awesome.min.css';
+import React from 'react';
+
+export default function LoadingBox(){
+    return(
+    <div>
+        <i className="fa fa-spinner fa-spin">Loading...</i>
+    </div>
+    );
+}
