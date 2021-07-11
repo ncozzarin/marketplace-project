c260b0d2c23e1a25a912317f312606875a1cd6ad
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-07-10 23:29:02 -0300

    Loading box element creation
    Loading box and home hook

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import Product from '../components/Product';
-import data from '../data';
 
-export default function HomeScreen(){
+
+export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = setState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        setLoading(true);
+      }catch(err){
+        setError(err.message);
+        setLoading(false);
+      }
+      setLoading(true);
+      const { data } = await axios.get('/api/products');
+      setLoading(true);
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
     return(
+      <div>
+        {loading? (<LoadingBox></LoadingBox>
+        ):
+        error?(<MessageBox>{error}</MessageBox>
+        ) : (
         <div className="row center">
-          {
-           data.products.map((product) => (
+           {products.map((product) => (
                 <Product key={product._id} product ={product}></Product>
-            )) 
-          }
-        </div>
+            ))}
+        </div> 
+        )}
+     </div>
     )
 }
