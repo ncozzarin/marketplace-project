b02e375e17687c8d2ec566a9ba2e1047a34212d7
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-07-14 18:19:15 -0300

    Provide default state to product list

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
+  const productList = useSelector((state) => state.productList || {});
   const { loading, error, products} = productList;
 
   useEffect(() => {
